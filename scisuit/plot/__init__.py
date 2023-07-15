f4a3296cc54100c82fb30f1381a6f1e5e5b2fd8d
Gökhan Bingöl <gbingol@hotmail.com>
2023-07-15 13:29:03 +0200

    kicking off plot library

---
+++ scisuit/plot/__init__.py
@@ -0,0 +1,120 @@
+import ctypes as _ct
+from ..util import parent_path as _parent_path
+
+
+_path = _parent_path(__file__) / "scisuit_plotter"
+_plt = _ct.PyDLL(str(_path))
+
+_plt.c_plot_scatter.argtypes = [_ct.py_object, _ct.py_object]
+_plt.c_plot_scatter.restype=_ct.py_object
+
+_plt.c_plot_psychrometry.argtypes = [_ct.py_object, _ct.py_object]
+_plt.c_plot_psychrometry.restype=_ct.py_object
+
+_plt.c_plot_app.argtypes = []
+_plt.c_plot_app.restype=_ct.py_object
+
+_plt.c_plot_mainloop.argtypes = [_ct.c_bool]
+_plt.c_plot_mainloop.restype=_ct.c_bool
+
+_plt.c_plot_ismainlooprunning.argtypes = []
+_plt.c_plot_ismainlooprunning.restype=_ct.c_bool
+
+_plt.c_plot_exitmainloop.argtypes = []
+_plt.c_plot_exitmainloop.restype=_ct.c_bool
+
+_plt.c_plot_close.argtypes = [_ct.py_object]
+_plt.c_plot_close.restype=_ct.c_bool
+
+
+def scatter(y, x:list=None, name:str=None, title:str=None, 
+	    xlab:str=None, ylab:str=None, smooth:bool=False, 
+	    bubble:dict=None, 
+	    marker:dict=None, line:dict=None, trendline:dict=None, hwnd=None):
+	"""
+	Plot scatter charts and returns a window handle
+
+	## Input:
+	x, y:	x- and y-data \n
+	name:	Name of the series \n
+	title: Title of the chart \n
+	xlab:	Label of x-axis \n
+	ylab:	Label of y-axis \n
+	smooth: Spline algorithm is applied to smooth the line \n
+	hwnd:	Window handle. If None, a new window is opened \n 
+
+	Bubble Properties \n
+	size:	size data (list), color: color (str), \n
+	mode: "A" area "W" diameter, scale: size scale (0, 200] \n
+
+
+	"""
+	_plt.c_plot_scatter
+	(
+		(), 
+		{'y':y ,"x":x, "name":name, "title":title, "xlab":xlab, "ylab":ylab, "smooth":smooth, 
+		"bubble":bubble, "marker":marker, "line":line, "trendline":trendline, "hwnd":hwnd}
+	)
+
+
+
+def psychrometry(Tdb:list=None, RH:list=None, P=101325):
+	"""
+	Plots psychromety chart.
+
+	## Input
+	Tdb: [min, max], minimum and maximum dry-bulb temperatures (Celcius) \n
+	RH: A list in increasing order containing the requested relative humidity (%) lines \n
+	P: Absolute pressure (Pa)
+	"""
+	_plt.c_plot_psychrometry
+	(
+		(), 
+		{'Tdb':Tdb, 'RH':RH, 'P':P}
+	)
+
+
+
+def app():
+	"""
+	Initiates a GUI application
+	"""
+	_plt.c_plot_app()
+
+
+def mainloop(shared=False):
+	"""
+	Once a GUI application is initiated, starts the main loop \n
+	shared: True, if there is any other application using a main loop \n
+
+	returns True if mainloop is started
+	"""
+	return _plt.c_plot_mainloop(_ct.c_bool(shared))
+
+
+def ismainlooprunning():
+	"""
+	Is there any main loop initiated by scisuit plot library running
+	"""
+	return _plt.c_plot_ismainlooprunning()
+
+
+def exitmainloop():
+	"""
+	Exits the main loop inititated by scisuit plot library and 
+	closes all the associated windows. \n
+
+	returns True if main loop is exited.
+	"""
+	return _plt.c_plot_exitmainloop()
+
+
+def close(hwnd):
+	"""
+	Closes the window with the given window handle \n
+	hwnd: Window handle \n
+
+	returns True if the window is closed
+	"""
+	return _plt.c_plot_close(hwnd)
+	
