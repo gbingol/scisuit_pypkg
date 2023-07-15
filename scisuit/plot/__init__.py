8ba58058d9b4f2a97af344c791a4b21334484785
Gökhan Bingöl <gbingol@hotmail.com>
2023-07-15 16:14:35 +0200

    boxplot added

--- scisuit/plot/__init__.py
+++ scisuit/plot/__init__.py
@@ -12,6 +12,9 @@ _plt.c_plot_bar.restype=_ct.py_object
 _plt.c_plot_barh.argtypes = [_ct.py_object, _ct.py_object]
 _plt.c_plot_barh.restype=_ct.py_object
 
+_plt.c_plot_boxplot.argtypes = [_ct.py_object, _ct.py_object]
+_plt.c_plot_boxplot.restype=_ct.py_object
+
 _plt.c_plot_scatter.argtypes = [_ct.py_object, _ct.py_object]
 _plt.c_plot_scatter.restype=_ct.py_object
 
@@ -36,80 +39,8 @@ _plt.c_plot_close.restype=_ct.c_bool
 
 
 
-def bar(height:list, labels=None, name=None, title=None, type="c", fill=None, line=None, hwnd=None):
-	"""
-	Plots bar chart
 
-	## Input
-	height : Numeric data \n
-	labels : Category labels \n
-	name:	Name of the series \n
-	type:	"c", "s" and "%" for clustered, stacked and 100% stacked, respectively.\n
-	title: Title of the chart ← string
-	"""
-	return _plt.c_plot_bar((),
-			{"height":height, "labels":labels, "name":name, "title":title, "type":type, 
-    			"fill":fill, "line":line, "hwnd":hwnd})
-
-
-
-def barh(width:list, labels=None, name=None, title=None, type="c", fill=None, line=None, hwnd=None):
-	"""
-	Plots horizontal bar chart
-
-	## Input
-	width : Numeric data \n
-	labels : Category labels \n
-	name:	Name of the series \n
-	type:	"c", "s" and "%" for clustered, stacked and 100% stacked, respectively.\n
-	title: Title of the chart ← string
-	"""
-	return _plt.c_plot_barh((),
-			{"width":width, "labels":labels, "name":name, "title":title, "type":type, 
-    			"fill":fill, "line":line, "hwnd":hwnd})
-
-
-
-def scatter(y, x:list=None, name:str=None, title:str=None, 
-	    xlab:str=None, ylab:str=None, smooth:bool=False, 
-	    bubble:dict=None, 
-	    marker:dict=None, line:dict=None, trendline:dict=None, hwnd=None):
-	"""
-	Plot scatter charts and returns a window handle
-
-	## Input:
-	x, y:	x- and y-data \n
-	name:	Name of the series \n
-	title: Title of the chart \n
-	xlab:	Label of x-axis \n
-	ylab:	Label of y-axis \n
-	smooth: Spline algorithm is applied to smooth the line \n
-	hwnd:	Window handle. If None, a new window is opened \n 
-
-	Bubble Properties \n
-	size:	size data (list), color: color (str), \n
-	mode: "A" area "W" diameter, scale: size scale (0, 200] \n
-
-
-	"""
-	return _plt.c_plot_scatter((), 
-		{'y':y ,"x":x, "name":name, "title":title, "xlab":xlab, "ylab":ylab, "smooth":smooth, 
-		"bubble":bubble, "marker":marker, "line":line, "trendline":trendline, "hwnd":hwnd})
-
-
-
-def psychrometry(Tdb:list=None, RH:list=None, P=101325):
-	"""
-	Plots psychromety chart.
-
-	## Input
-	Tdb: [min, max], minimum and maximum dry-bulb temperatures (Celcius) \n
-	RH: A list in increasing order containing the requested relative humidity (%) lines \n
-	P: Absolute pressure (Pa)
-	"""
-	return _plt.c_plot_psychrometry((),{'Tdb':Tdb, 'RH':RH, 'P':P})
-
-
+#---------------------------------------------------------------
 
 def app():
 	"""
@@ -156,3 +87,97 @@ def close(hwnd):
 	"""
 	return _plt.c_plot_close(hwnd)
 	
+
+
+
+
+
+
+#-----------------------------------------------------
+
+def bar(height:list, labels=None, name=None, title=None, type="c", fill=None, line=None, hwnd=None):
+	"""
+	Plots bar chart
+
+	## Input
+	height : Numeric data \n
+	labels : Category labels \n
+	name:	Name of the series \n
+	type:	"c", "s" and "%" for clustered, stacked and 100% stacked, respectively.\n
+	title: Title of the chart
+	"""
+	return _plt.c_plot_bar((),
+			{"height":height, "labels":labels, "name":name, "title":title, "type":type, 
+    			"fill":fill, "line":line, "hwnd":hwnd})
+
+
+
+def barh(width:list, labels=None, name=None, title=None, type="c", fill=None, line=None, hwnd=None):
+	"""
+	Plots horizontal bar chart
+
+	## Input
+	width : Numeric data \n
+	labels : Category labels \n
+	name:	Name of the series \n
+	type:	"c", "s" and "%" for clustered, stacked and 100% stacked, respectively.\n
+	title: Title of the chart
+	"""
+	return _plt.c_plot_barh((),
+			{"width":width, "labels":labels, "name":name, "title":title, "type":type, 
+    			"fill":fill, "line":line, "hwnd":hwnd})
+
+
+
+def boxplot(data:list, name:str=None, title:str=None, fill:dict=None, line:dict=None, hwnd=None):
+	"""
+	Plots box-whisker chart and returns a window handle.
+
+	## Input
+	data : Data to be plotted \n
+	name:	Name of the series \n
+	title: Title of the chart 
+	"""
+	return _plt.c_plot_boxplot((),
+			{"data":data, "name":name, "title":title, "fill":fill, "line":line, "hwnd":hwnd})
+
+
+
+
+def psychrometry(Tdb:list=None, RH:list=None, P=101325):
+	"""
+	Plots psychromety chart.
+
+	## Input
+	Tdb: [min, max], minimum and maximum dry-bulb temperatures (Celcius) \n
+	RH: A list in increasing order containing the requested relative humidity (%) lines \n
+	P: Absolute pressure (Pa)
+	"""
+	return _plt.c_plot_psychrometry((),{'Tdb':Tdb, 'RH':RH, 'P':P})
+
+
+
+
+def scatter(y, x:list=None, name:str=None, title:str=None, 
+	    xlab:str=None, ylab:str=None, smooth:bool=False, 
+	    bubble:dict=None, 
+	    marker:dict=None, line:dict=None, trendline:dict=None, hwnd=None):
+	"""
+	Plot scatter charts and returns a window handle
+
+	## Input:
+	x, y:	x- and y-data \n
+	name:	Name of the series \n
+	title: Title of the chart \n
+	xlab:	Label of x-axis \n
+	ylab:	Label of y-axis \n
+	smooth: Spline algorithm is applied to smooth the line \n
+	hwnd:	Window handle. If None, a new window is opened \n 
+
+	Bubble Properties \n
+	size:	size data (list), color: color (str), \n
+	mode: "A" area "W" diameter, scale: size scale (0, 200]
+	"""
+	return _plt.c_plot_scatter((), 
+		{'y':y ,"x":x, "name":name, "title":title, "xlab":xlab, "ylab":ylab, "smooth":smooth, 
+		"bubble":bubble, "marker":marker, "line":line, "trendline":trendline, "hwnd":hwnd})
