2db44216022ad5f23f0cd6f7215d9c21abd1d528
Gökhan Bingöl <gbingol@hotmail.com>
2023-07-16 15:13:26 +0200

    tests for line chart

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -8,7 +8,6 @@ import wx
 import scisuit.plot as plt
 
 import numpy as np
-import scisuit.stats as stat
 
 
 
@@ -16,16 +15,46 @@ plt.app()
 
 
 
+
 plt.mainloop()
 
 
+
+
+
+
 """
-#Test Histogram
+#Histogram
+
+import scisuit.stats as stat
 
 x=stat.rnorm(500)
 
 plt.histogram(x)
 plt.histogram(x, mode=plt.Histogram_Mode.RELFREQUENCY, cumulative=True)
 plt.histogram(x, fill={'color': plt.Color.RED}, line={'color': plt.Color.GREEN, 'width': 2} )
+"""
 
+
+"""
+#Line Chart
+
+categories=["Q1", "Q2", "Q3", "Q4"]
+A = [44, 55, 41, 67]
+B = [13, 23, 8, 13]
+
+#Line (Unstacked)
+plt.line(labels=categories, y=A)
+
+#Clustered line chart with two series
+hwnd = plt.line(labels=categories, y=A)
+plt.line(y=B, hwnd=hwnd)
+
+#Stacked line chart with two series
+hwnd = plt.line(labels=categories, y=A, type=plt.Line_Type.STACKED)
+plt.line(y=B, type=plt.Line_Type.STACKED, hwnd=hwnd)
+
+#%-stacked with two series
+hwnd = plt.line(labels=categories, y=A, type=plt.Line_Type.PERCENTSTK)
+plt.line(y=B, type=plt.Line_Type.PERCENTSTK, hwnd=hwnd)
 """
