13b2620aa45cc70b2da6574eaf1c3cb63e8f6539
Gökhan Bingöl <gbingol@hotmail.com>
2023-07-18 21:21:25 +0200

    r22 and r23 added

--- tests/test_fluids.py
+++ tests/test_fluids.py
@@ -3,12 +3,7 @@ import sys, os
 #inserting to 0th position is very important so that search will FIRST match ../scisuit folder
 sys.path.insert(0, os.getcwd()) 
 
-from scisuit.eng.fluids.fluids_dbase import ThermoPhysical, SaturatedRefrigerant
+from scisuit.eng.fluids.refrigerants import Refrigerant
 
-f = ThermoPhysical("water")
-res = f.search("T", 20)
-print(res)
-
-ref = SaturatedRefrigerant("water")
-res = ref.search("T", 30)
-print(res)
+r = Refrigerant.R12()
+print(r.hf[2])
