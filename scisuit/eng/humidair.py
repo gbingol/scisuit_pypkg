41701af8bd08b4e86ddeca8f23d448d91be3e95f
Gökhan Bingöl <gbingol@hotmail.com>
2023-08-29 21:32:48 +0200

    edited docstrings

--- scisuit/eng/humidair.py
+++ scisuit/eng/humidair.py
@@ -16,106 +16,76 @@ class PsychrometryResult:
 		self.m_Results = psyresult
 
 	def __str__(self) -> str:
-		retStr=""
 
-		if(len(self.m_Results)==0):
-			return retStr
+		if(len(self.m_Results) == 0):
+			return ""
+		
+		s=""
+		s += "P=" + str(round(self.P, 2)) + " kPa" + "\n"
+		s += "Tdb=" + str(round(self.Tdb, 2)) + " C" + "\n"	
+		s += "Twb=" + str(round(self.Twb, 2)) + " C" + "\n"	
+		s += "Tdp=" + str(round(self.Tdp, 2)) +  " C" + "\n"	
+		s += "H=" + str(round(self.H, 2)) + " kJ/kg da" + "\n"	
+		s += "RH=" + str(round(self.RH, 2)) + " %" + "\n"	
+		s += "W=" + str(round(self.W, 4)) +  " kg/kg da" + "\n"	
+		s += "V=" + str(round(self.V,3)) +  " m3/kg da" + "\n"
 
-		retStr += "P=" + str(round(self.P, 2)) + " kPa" + "\n"
-
-		retStr += "Tdb=" + str(round(self.Tdb, 2)) + " C" + "\n"
-		
-		retStr += "Twb=" + str(round(self.Twb, 2)) + " C" + "\n"
-		
-		retStr += "Tdp=" + str(round(self.Tdp, 2)) +  " C" + "\n"
-		
-		retStr += "H=" + str(round(self.H, 2)) + " kJ/kg da" + "\n"
-		
-		retStr += "RH=" + str(round(self.RH, 2)) + " %" + "\n"
-		
-		retStr += "W=" + str(round(self.W, 4)) +  " kg/kg da" + "\n"
-		
-		retStr += "V=" + str(round(self.V,3)) +  " m3/kg da" + "\n"
-
-		return retStr
+		return s
 
 
-            
 	@property
 	def P(self): 
 		"""Pressure in kPa"""
-		
 		return self.m_Results.get("P")
-
 
 	@property
 	def Tdb(self): 
-		"""
-		Dry bulb temperature in Celcius
-		"""
+		"""Dry bulb temperature in Celcius"""
 		return self.m_Results.get("Tdb")
 
 	@property
 	def Twb(self): 
-		"""
-		Wet bulb temperature in Celcius
-		"""
+		"""Wet bulb temperature in Celcius"""
 		return self.m_Results.get("Twb")
 
 	@property
 	def Tdp(self): 
-		"""
-		Dew point temperature in Celcius
-		"""
+		"""Dew point temperature in Celcius"""
 		return self.m_Results.get("Tdp")
 
 	@property
 	def H(self):
-		"""
-		Enthalpy in kJ/kg da
-		""" 
+		"""Enthalpy in kJ/kg da""" 
 		return self.m_Results.get("H")
 
 	@property
 	def RH(self): 
-		"""
-		Relative Humidity in %
-		"""
+		"""Relative Humidity in %"""
 		return self.m_Results.get("RH")
 
 	@property
 	def W(self):
-		"""
-		Absolute Humidity in kg/kg da
-		""" 
+		"""Absolute Humidity in kg/kg da""" 
 		return self.m_Results.get("W")
 
 	@property
 	def V(self): 
-		"""
-		Specific volume in m3/kg da
-		"""
+		"""Specific volume in m3/kg da"""
 		return self.m_Results.get("V")
 
 	@property
 	def Pws(self):
-		"""
-		water vapor pressure at saturation in kPa
-		"""
+		"""water vapor pressure at saturation in kPa"""
 		return self.m_Results.get("Pws")
 
 	@property
 	def Pw(self):
-		"""
-		water vapor pressure in kPa
-		"""
+		"""water vapor pressure in kPa"""
 		return self.m_Results.get("Pw")
 
 	@property
 	def Ws(self):
-		"""
-		Absolute humidity at saturation kg/kg da
-		"""
+		"""Absolute humidity at saturation kg/kg da"""
 		return self.m_Results.get("Ws")
 
 
