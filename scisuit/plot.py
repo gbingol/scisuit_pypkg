323a74c050afb5f478c26c0a19777242d3346ac6
Gökhan Bingöl <gbingol@hotmail.com>
2023-09-08 14:10:49 +0200

    assertion for label type

--- scisuit/plot.py
+++ scisuit/plot.py
@@ -490,13 +490,17 @@ def figure():
 
 def title(label:str):
 	"""Create chart title"""
+	assert isinstance(label, str), "label must be of type string."
 	pltdll.c_plot_title(label)
 
 
 def xlabel(label:str):
 	"""Create x-axis label"""
+	assert isinstance(label, str), "label must be of type string."
 	pltdll.c_plot_xlabel(label)
+
 
 def ylabel(label:str):
 	"""Create y-axis label"""
+	assert isinstance(label, str), "label must be of type string."
 	pltdll.c_plot_ylabel(label)
