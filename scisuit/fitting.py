118922adccec5ac8e56cd35f3902e76489339395
Gökhan Bingöl <gbingol@hotmail.com>
2023-09-13 17:58:54 +0200

    changed spline functions return type for clarity

--- scisuit/fitting.py
+++ scisuit/fitting.py
@@ -1,10 +1,22 @@
 from ._ctypeslib import coreDLL as _core
 import ctypes as _ct
+import dataclasses as _dc
 import numbers as _numbers
 from numpy.polynomial import polynomial as _Polynomial
+from typing import Iterable
+
+__all__ = ['linearinterp', 'lagrange', 'spline', 'expfit', 'logfit', 'logistfit', 'polyfit', 'powfit', 'SplineResult']
 
 
-__all__ = ['linearinterp', 'lagrange', 'spline', 'expfit', 'logfit', 'logistfit', 'polyfit', 'powfit']
+@_dc.dataclass
+class SplineResult:
+	"""
+	poly: Numpy polynomial
+	lower, upper: lower and upper bounds of the polynomial
+	"""
+	poly:_Polynomial.Polynomial = None
+	lower:float = None
+	upper: float = None
 
 
 
@@ -34,12 +46,12 @@ def linearinterp(x1:float, y1:float, x2:float, y2:float, xval:float)->float:
 
 
 
-def lagrange(x, y, val:float)->float:
+def lagrange(x:Iterable, y:Iterable, val:float)->float:
 	"""
 	Constructs lagrange polynomial from x,y to compute the given value.
 
 	## Inputs:
-	x, y: list/ndarray \n
+	x, y: x and y-values \n
 	val: value in the range of x whose corresponding y value is wish to be known.
 	"""
 	assert issubclass(val, _numbers.Real)
@@ -47,21 +59,13 @@ def lagrange(x, y, val:float)->float:
 	return _core.c_fit_lagrange(x, y, _ct.c_double(val))
 
 
-def spline(x, y)->list:
+def spline(x:Iterable, y:Iterable)->list[SplineResult]:
 	"""
 	Constructs natural cubic spline polynomials from x, y
-
-	## Inputs:
-	x, y: list/ndarray
-
-	## Return:
-	Returns a 2D Python list, where each sub-list contains exactly 3 entries: \n
-	1) List of coefficients of the cubic polynomial,
-	2) Lower bound,
-	3) Upper bound.
 	"""
-	return _core.c_fit_spline(x, y)
-
+	lst = _core.c_fit_spline(x, y)
+	
+	return  [SplineResult(_Polynomial.Polynomial(l[0]), l[1], l[2]) for l in lst]
 
 
 def expfit(x, y, intercept=None)->list:
@@ -83,25 +87,22 @@ def expfit(x, y, intercept=None)->list:
 	return _core.c_fit_expfit(x, y, intercept)
 
 
-def logfit(x, y)->list:
+def logfit(x:Iterable, y:Iterable)->list:
 	"""
 	Fits x,y to the equation y = a*ln(x) + b \n
 	return list is [a, b]
 
-	## Inputs:
-	x, y: list/ndarray
 	"""
 
 	return _core.c_fit_logfit(x, y)
 
 
-def logistfit(x, y, limit = None)->list:
+def logistfit(x:Iterable, y:Iterable, limit = None)->list:
 	"""
 	Fits x,y to the equation y = L / (1 + exp(b0 + b1*x)) \n
 	return list is [L, b0, b1] or [b0, b1]
 
 	## Inputs:
-	x, y: list/ndarray \n
 	limit: None or float
 	"""
 	if(limit!=None):
@@ -123,13 +124,9 @@ def polyfit(x, y, deg)->tuple:
 	return coeffs, stats[0]
 
 
-def powfit(x, y)->list:
+def powfit(x:Iterable, y:Iterable)->list:
 	"""
 	Fits x,y to the equation y = a*x^n
 	returns [a, n]
-
-	## Inputs:
-	x, y: list/ndarray
 	"""
 	return _core.c_fit_powfit(x,y)
-
---
+++ tests/test_fitting.py
@@ -0,0 +1,15 @@
+import sys, os
+import numpy as np
+
+#inserting to 0th position is very important so that search will FIRST match ../scisuit folder
+sys.path.insert(0, os.getcwd()) 
+
+
+import scisuit.fitting as fit
+x = [0, 1, 2, 3]
+y = [1, -8, -30, -59]
+
+t = fit.spline(x,y)
+print(t[0].poly)
+
+print(t)
