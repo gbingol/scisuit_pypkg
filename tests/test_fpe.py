365c4a1b8de979325797da88c0b6bfddf6856539
Gökhan Bingöl <gbingol@hotmail.com>
2023-10-02 11:58:39 +0200

    fixed error

--- tests/test_fpe.py
+++ tests/test_fpe.py
@@ -6,12 +6,13 @@ sys.path.insert(0, os.getcwd())
 
 import scisuit.eng.fpe as fpe
 
-milk = fpe.Food(water=88.13, protein=3.15, cho=4.8, lipid=3.25, ash=0.67)
-print(milk)
+milk = fpe.Food(water=88.13, protein=3.15, cho=4.80, lipid=3.25, ash=0.67)
+water = fpe.Food(water=100)
 
-f1= fpe.Beverage (water =80, cho=20)
-f2= fpe.Meat (water =60, cho=40)
+#removal of 87% water from milk (Food - Food = Food)
+powder = milk - 0.87*water
+print(powder)
 
-c = f1 + f2
-print(c)
-
+#convert general powder into a dairy powder
+dairy_powder = fpe.Dairy(**powder.ingredients())
+print(dairy_powder)
