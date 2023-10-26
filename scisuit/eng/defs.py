a4eed2a974b198696e3c4a1bf0bd79eed80ceaa4
Gökhan Bingöl <gbingol@hotmail.com>
2023-10-26 14:53:20 +0200

    fixed bug in dielectric

--- scisuit/eng/defs.py
+++ scisuit/eng/defs.py
@@ -1,6 +1,21 @@
-import dataclasses as _dc
 
-@_dc.dataclass
 class Dielectric:
-	Constant =0.0
-	Loss = 0.0
+	def __init__(self, constant, loss) -> None:
+		self._Constant =constant
+		self._Loss = loss
+	
+	@property
+	def constant(self):
+		pass
+
+	@constant.getter
+	def constant(self):
+		return self._Constant
+	
+	@property
+	def loss(self):
+		pass
+
+	@loss.getter
+	def loss(self):
+		return self._Loss
