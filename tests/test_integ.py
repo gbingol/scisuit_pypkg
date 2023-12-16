2dc73b7ddb18a10806e456fe4135a5e3f5e0052b
Gökhan Bingöl <gbingol@hotmail.com>
2023-12-16 11:01:20 +0300

    testing new integ funcs

--- tests/test_integ.py
+++ tests/test_integ.py
@@ -4,7 +4,7 @@ import numpy as np
 #inserting to 0th position is very important so that search will FIRST match ../scisuit folder
 sys.path.insert(0, os.getcwd()) 
 
-from scisuit.integ import trapz, cumtrapz
+from scisuit.integ import trapz, cumtrapz, simpson, romberg, fixed_quad
 
 x = np.arange(1, 6)
 y = x**2
@@ -13,9 +13,10 @@ print("****** cumtrapz ******")
 print(cumtrapz(x, y))
 
 
-print("****** trapz ******")
-
 x=np.linspace(0, 3, num=100)
 y = x**2
 
 print(trapz(x,y))
+print(simpson(x, y))
+print(romberg(lambda x: x**2, 0, 3))
+print(fixed_quad(lambda x: x**2, 0, 3))
