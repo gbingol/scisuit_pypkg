500e6ef66c91aedc1ee386d7c2c117331d8f66f3
Gökhan Bingöl <gbingol@hotmail.com>
2024-02-13 12:50:47 +0100

    post_init for dataclasses to check params

--- scisuit/plot/gdi.py
+++ scisuit/plot/gdi.py
@@ -7,8 +7,25 @@ class Pen:
 	width:int = 1
 	style:int= 100 #solid pen
 
+	def __post_init__(self):
+		if self.color != None:
+			assert isinstance(self.color, str), "'color' must be string"
+
+		assert isinstance(self.style, int), "'style' must be integer"
+		assert self.style>0, "style>0 expected"
+
+		assert isinstance(self.width, int), "'width' must be integer"
+		assert self.width>0, "width>0 expected"
+
 
 @_dc.dataclass
 class Brush:
 	color:str = None
 	style:int = 100 #solid brush
+
+	def __post_init__(self):
+		if self.color != None:
+			assert isinstance(self.color, str), "'color' must be string"
+
+		assert isinstance(self.style, int), "'style' must be integer"
+		assert self.style>0, "style>0 expected"
