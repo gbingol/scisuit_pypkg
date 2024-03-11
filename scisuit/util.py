2c2ab2f066b9aa39f5e4d7549bc5f90a4b6d0b74
Gökhan Bingöl <gbingol@hotmail.com>
2024-03-11 16:59:45 +0100

    added NiceNumbers class

--- scisuit/util.py
+++ scisuit/util.py
@@ -1,33 +1,57 @@
-import os as _os
-import sys as _sys
-from pathlib import Path as _Path
+import math
 
 
 
-def parent_path(path:str, level = 0)->_Path:
-	"""
-	This is used so many times that a convenience function is deemed appropriate.
+class NiceNumbers:
+	def __init__(self, minimum, maximum, maxticks=10) -> None:
+		self._min = minimum
+		self._max = maximum
+		self._maxticks = maxticks
 
-	## Input: 
-	path: Relative or full path 
+		self._calculate()
 	
-	## Example:
-	if path is C:\\a\\b\\c.py  \n
-	level=0 => C:\\a\\b \n
-	level=1 => C:\\a
+
+	def _calculate(self):
+		_range = self._nicenum(self._max - self._min, False)
+		self._tickspacing = self._nicenum(_range / (self._maxticks - 1), True)
+
+		self._nicemin = math.floor(self._min / self._tickspacing) * self._tickspacing
+		self._nicemax = math.ceil(self._max / self._tickspacing) * self._tickspacing
+
+
+	def _nicenum(self, Range:float, Round:bool):
+		exponent = math.floor(math.log10(Range))
+		frac = Range / math.pow(10, exponent)
+
+		if Round:
+			if frac < 1.5: niceFrac = 1
+			elif frac < 3: niceFrac = 2
+			elif frac < 7: niceFrac = 5
+			else: niceFrac = 10
+		else:
+			if frac <= 1: niceFrac = 1
+			elif frac <= 2: niceFrac = 2
+			elif frac <= 5: niceFrac = 5
+			else: niceFrac = 10
+
+		return niceFrac * math.pow(10.0, exponent)
+
+	@property
+	def nicemin(self):
+		return self._nicemin
+
+	@property
+	def nicemax(self):
+		return self._nicemax
 	
-	"""
-	pt = _Path(path)
+	@property
+	def tickspace(self):
+		return self._tickspacing
+	
 
-	if(pt.is_absolute() == False):
-		pt = pt.absolute()
-
-	return pt.parents[level]
-
-
-
-def pyhomepath()->str:
-	"""
-	returns the Python Home Path
-	"""
-	return _sys.exec_prefix
+"""
+if __name__ == "__main__":
+	n = NiceNumbers(0.5, 10.25)
+	print(n.nicemax, " ", n.nicemin, " ", n.tickspace)
+	
+"""
