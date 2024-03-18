ad6c21cbf6611d85afddfc182212dba94e8c125f
Gökhan Bingöl <gbingol@hotmail.com>
2024-03-18 21:14:26 +0100

    work on bar chart

---
+++ scisuit/plot/barcharts.py
@@ -0,0 +1,9 @@
+import numpy as np
+from typing import Iterable as _Iterable
+
+from .gdi import rect
+from .charts import canvas
+from ..util import NiceNumbers, minmax
+
+def bar(x, height, width=0.8):
+	pass
