6b2f0747418af3fef619a8bf5f5e443ffc397811
Gökhan Bingöl <gbingol@hotmail.com>
2024-04-03 12:31:39 +0200

    perf comparison with scipy

--- tests/rootfinding.py
+++ tests/rootfinding.py
@@ -1,5 +1,7 @@
 import sys, os
-import pprint
+import timeit
+import scipy
+import scipy.optimize
 
 #inserting to 0th position is very important so that search will FIRST match ../scisuit folder
 sys.path.insert(0, os.getcwd()) 
@@ -9,16 +11,7 @@ from scisuit.roots import bisect, brentq, Info
 def func(x):
     return x**2-5
 
-root, info = bisect(f=func, a=0, b=5)
 
-print("**** Bisection method ****")
-print(root," ", info)
 
-root, info = brentq(f=func, a=0, b=5)
-
-print("\n **** Brent's method ****")
-print(root," ", info)
-
-from scisuit.roots import bisect
-root, info = bisect(lambda x: x**2-5, a = 0, b = 4)
-print(root, " ",  info)
+print("Scipy:", timeit.timeit(lambda: scipy.optimize.brentq(f=func, a=0, b=5), number=1000))
+print("scisuit:", timeit.timeit(lambda: brentq(f=func, a=0, b=5, tol=1E-16), number=1000))
