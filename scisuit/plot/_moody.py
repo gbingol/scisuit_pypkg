3f5005996858bbd86fac498b23a76060eab69f32
Gökhan Bingöl <gbingol@hotmail.com>
2024-04-13 22:34:05 +0200

    good progress on moody's chart

--- scisuit/plot/_moody.py
+++ scisuit/plot/_moody.py
@@ -1,6 +1,18 @@
+import math
+
+from ..roots import brentq
 from ._charts import canvas, xscale
 from .gdi import marker, line, curve
 from ..util import NiceNumbers
+
+
+def _Colebrook(f, Re, E_D):
+	#inside log
+	temp1 = E_D/3.7 + 2.51/(Re*math.sqrt(f)) 
+	temp2 = 2.0* math.log10(temp1) 
+
+	return 1/math.sqrt(f) + temp2
+
 
 
 def moody():
@@ -15,3 +27,19 @@ def moody():
 	p1 = (1000, fD(1000))
 	p2 = (2300, fD(2300))
 	line(p1=p1, p2=p2, lw=2, ls="--")
+
+	#Turbulent Region - Re>=4000
+	Re = [4000, 7000, 14000, 30000, 60000, 120E3, 250E3, 1E6, 5E6]
+
+	e_d = [0.005, 0.01, 0.02]
+
+	for _e_d in e_d:
+		x, y = [], []
+		for re in Re:
+			_cb = lambda f: _Colebrook(f, re, _e_d)
+			friction, _ = brentq(_cb, a=0.001, b=0.08)
+			x.append(re)
+			y.append(friction)
+		curve(x, y)
+
+		
