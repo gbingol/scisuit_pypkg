8b57e1891ab3a4b1346cbbffb7f112fb2e5f0b5a
Gökhan Bingöl <gbingol@hotmail.com>
2024-04-16 14:13:40 +0200

    minor changes

--- tests/rootfinding.py
+++ tests/rootfinding.py
@@ -9,17 +9,15 @@ import scipy.optimize as opt
 from scisuit.roots import brentq, toms748
 from timeit import timeit
 
-def f(x):
-    return x**2-5
 
-N=1000
+kwargs, N = {"f": lambda x: x**2-5, "a":0, "b":5}, 1000
 
 print("Scipy functions")
-print("brentq:", timeit(lambda: opt.brentq(f=f, a=0, b=5), number=N))
-print("toms748:", timeit(lambda: opt.toms748(f=f, a=0, b=5), number=N))
+print("brentq:", timeit(lambda: opt.brentq(**kwargs), number=N))
+print("toms748:", timeit(lambda: opt.toms748(**kwargs), number=N))
 
 print("scisuit functions")
-print("brentq:", timeit(lambda: brentq(f=f, a=0, b=5, tol=1E-16), number=N))
-print("toms748:", timeit(lambda: toms748(f=f, a=0, b=5, tol=1E-16), number=N))
+print("brentq:", timeit(lambda: brentq(**kwargs, tol=1E-16), number=N))
+print("toms748:", timeit(lambda: toms748(**kwargs, tol=1E-16), number=N))
 
-print("Root:", toms748(f=f, a=0, b=5, tol=1E-6))
+print("Root:", toms748(**kwargs, tol=1E-6))
