c7005028ebe98b532f1458f7491770415963281d
Gökhan Bingöl <gbingol@hotmail.com>
2024-04-29 10:31:21 +0200

    changes and improvements to moody diagram

--- scisuit/plot/_moody.py
+++ scisuit/plot/_moody.py
@@ -12,12 +12,12 @@ def _Colebrook(f, Re, E_D):
 	return 1/math.sqrt(f) + temp2
 
 
-def _TurbulenceOnset(E_D):	
+def _TurbulenceOnset(E_D:float):	
 	"""
 	compute friction factor
 	A modified version of Colebrook's equation for large Re
 	"""
-	def _vonKarman(E_D):
+	def _vonKarman(E_D:float):
 		f = -2.0* math.log10(E_D/3.7) 
 		return (1/f)**2
 	
@@ -25,7 +25,7 @@ def _TurbulenceOnset(E_D):
 	compute Reynolds number
 	Transactions of the ASME, 66 (8): 671-684 (Page: 676)
 	"""
-	def _Reynolds(f):
+	def _Reynolds(f:float):
 		return 1/math.sqrt(f)*200/E_D
 	
 	f = _vonKarman(E_D)
@@ -36,22 +36,16 @@ def _TurbulenceOnset(E_D):
 
 def moody(
 		Re:Iterable = [1E3, 1E7],
-		friction:Iterable = [0.01, 0.09],
-		showlaminar = True,
-		showtransition = True,
-		showturbulent = True):
+		friction:Iterable = [0.01, 0.09]):
 	"""
 	`Re:` Reynolds number range
 	`friction:`  Friction factor range
-	`showlaminar:` show laminar line and related info
-	`showtransition:` show transition region and related info
-	`showturbulent:` show fully turbulent line and related info
 	"""
-	assert isinstance(Re, Iterable), "Re must be iterable"
-	assert len(Re)==2, "Re must have length 2"
+	assert isinstance(Re, Iterable), "Re must be iterable."
+	assert len(Re)==2, "Re must have length 2."
 
-	assert isinstance(friction, Iterable), "friction must be iterable"
-	assert len(friction)==2, "friction must have length 2"
+	assert isinstance(friction, Iterable), "friction must be iterable."
+	assert len(friction)==2, "friction must have length 2."
 
 	canvas(
 		x=(float(Re[0]), float(Re[1])), 
@@ -62,23 +56,22 @@ def moody(
 	"""
 	laminar flow 0<Re<2300
 	"""
-	if showlaminar:
-		fD = lambda _Re: 64.0/_Re
-		p1 = (1000, fD(1000))
-		p2 = (2300, fD(2300))
-		line(p1=p1, p2=p2, lw=2, ls="--", ec="#FF0000", label="laminar")
-		text(xy=p2, label="Laminar Flow\nf = 64/Re", hanchor="c")
+	fD = lambda _Re: 64.0/_Re
+	p1 = (1000, fD(1000))
+	p2 = (2300, fD(2300))
+	idline = line(p1=p1, p2=p2, lw=2, ls="--", ec="#FF0000", label="laminar")
+	idtxt = text(xy=p2, label="Laminar Flow\nf = 64/Re", hanchor="c")
+	setvisibility(idline, [idtxt])
 
 	"""
 	transition region 2300<Re<4000
 	btmleft: x (onset of transition), y(roughly 64/Re)
 	rectangle's width = 4000-2300=1700, height=0.08-0.03
 	"""
-	if showtransition:
-		btmleft = (2300, 0.03)
-		idrect = rect(xy=btmleft, width=1700, height=0.05, hatch="solid", alpha=0.5, ls="--", fc="#808080", label="transition")
-		idtext = text(xy=(3300, 0.06), label="Transition Region", rotation=-90, labelcolor="#A52A2A")
-		setvisibility(idrect, [idtext])
+	btmleft = (2300, 0.03)
+	idrect = rect(xy=btmleft, width=1700, height=0.05, hatch="solid", alpha=0.5, ls="--", fc="#808080", label="transition")
+	idtext = text(xy=(3300, 0.06), label="Transition Region", rotation=-90, labelcolor="#A52A2A")
+	setvisibility(idrect, [idtext])
 
 	"""
 	Turbulent Region - Re>=4000
@@ -96,23 +89,25 @@ def moody(
 			x.append(_Re)
 			y.append(friction)
 
-		text(xy=(_Re, friction), label=str(_e_d), vanchor="c")
-		curve(x, y, lw=2, ec="#A52A2A", label=str(_e_d))
+		id1 = text(xy=(_Re, friction), label=str(_e_d), vanchor="c")
+		idcurve = curve(x, y, lw=2, ec="#A52A2A", label=str(_e_d))
+		setvisibility(idcurve, [id1])
 
 	"""
 	fully rough turbulent flow line
 	E/D=0.06 appended so that the dashed line will be slightly above E/D=0.05 line
 	"""
-	if showturbulent:
-		TurbulenceLine.append(_TurbulenceOnset(0.06))
-		_turbulence = list(zip(*TurbulenceLine))
-		curve(x=_turbulence[0], y=_turbulence[1], lw=2, ls="--")
-		
-		midpoint = int(len(TurbulenceLine)/2)
-		xx, yy=_turbulence[0][midpoint], _turbulence[1][midpoint]
-		_p1 = (15E3, 0.015)
-		arrow(p1=_p1, p2=(xx,yy), lw=2, length=0.05, label="arrow")
-		text(xy=_p1, label="Complete Turbulence", hanchor="c")
+	TurbulenceLine.append(_TurbulenceOnset(0.06))
+	_turbulence = list(zip(*TurbulenceLine))
+	idturbcurve = curve(x=_turbulence[0], y=_turbulence[1], lw=2, ls="--", label="complete turb")
+	
+	midpoint = int(len(TurbulenceLine)/2)
+	xx, yy=_turbulence[0][midpoint], _turbulence[1][midpoint]
+	_p1 = (15E3, 0.015)
+	idturbarr = arrow(p1=_p1, p2=(xx,yy), lw=2, length=0.05)
+	idturbtxt = text(xy=_p1, label="Complete Turbulence", hanchor="c")
+	setvisibility(idturbcurve, [idturbtxt, idturbarr])
+
 
 	xlabel("Reynolds Number")
 	ylabel("Friction Factor")
