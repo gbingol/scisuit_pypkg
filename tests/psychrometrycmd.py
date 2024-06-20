a8db6dd34f7e874c952c0b9a2b27b3d2c50542c4
Gökhan Bingöl <gbingol@hotmail.com>
2024-06-20 11:22:19 +0200

    improvements to PHRH combination

--- tests/psychrometrycmd.py
+++ tests/psychrometrycmd.py
@@ -10,7 +10,7 @@ from scisuit.eng import psychrometry
 result = psychrometry(P=101, Tdb=70, Twb=40)
 
 #testing new solver method
-result2 = psychrometry(RH=result.RH, Twb=result.Twb, P=result.P)
+result2 = psychrometry(RH=result.RH, H=result.H, P=result.P)
 
 #all of the properties
 print(result)
