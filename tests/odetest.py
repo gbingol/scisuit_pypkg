0ed0db688be53d2f60004be6e1a72ffda0dc370a
Gökhan Bingöl <gbingol@hotmail.com>
2024-06-23 19:30:03 +0200

    tests on ivp

--- tests/odetest.py
+++ tests/odetest.py
@@ -1,9 +1,10 @@
+import math
 import numpy as np
 from scipy.integrate import solve_ivp
 
 def f(x, y): 
-	return -2*x**3 + 12*x**2 - 20*x + 8.5
+	return 4*math.exp(0.8*x)-0.5*y
 
-sol = solve_ivp(f, [0, 4], [1], t_eval=np.arange(0.0, 4.5, 0.5))
+sol = solve_ivp(f, [0, 2], [2])
 print(sol.t)
 print(sol.y)
