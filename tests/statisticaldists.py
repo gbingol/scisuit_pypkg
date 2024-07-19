5a03ace58ea6e08ced21fc06b7f4db84fcef1fe7
Gökhan Bingöl <gbingol@hotmail.com>
2024-07-19 14:20:59 +0200

    better testing of stat dists

--- tests/statisticaldists.py
+++ tests/statisticaldists.py
@@ -9,14 +9,24 @@ sys.path.insert(0, os.getcwd())
 import scisuit.stats as st
 
 
+def beta():
+	from scisuit.stats import dbeta, pbeta, qbeta
 
-def GammaDist():
-	print(st.dgamma(x=4, scale=4, shape=3))
-	print(st.pgamma(q = 3, shape = 10, scale = 0.25))
-	print(st.qgamma(p =[0, 0.1, 0.2], shape = 10, scale = 0.25))
+	print("\n Beta Dist")
+	print(dbeta(x=0.1, shape1=0.5, shape2=1))
+	print(pbeta(q=0.1, shape1=0.5, shape2=1))
+	print(qbeta(p=0.1, shape1=0.5, shape2=1))
+
+
+def BinomialDist():
+	print("\n Binomial Dist")
+	print(st.dbinom(x=2, size=5, prob=0.3))
+	print(st.dbinom(x=[7, 8, 9], size=9, prob=0.94))
+	print(st.qbinom(p=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6], size=9, prob=0.65))
 
 
 def ExponentialDist():
+	print("\n Exponential Dist")
 	print(st.dexp(x=3, rate=0.2))
 	print(st.pexp(q=3, rate=0.2))
 	print(st.qexp(p=0.3, rate=0.2))
@@ -26,23 +36,16 @@ def ExponentialDist():
 	print(st.qexp(p=[0.1, 0.2, 0.3], rate=0.2))
 
 
+def GammaDist():
+	print("\n Gamma Dist")
+	print(st.dgamma(x=4, scale=4, shape=3))
+	print(st.pgamma(q = 3, shape = 10, scale = 0.25))
+	print(st.qgamma(p =[0, 0.1, 0.2], shape = 10, scale = 0.25))
 
-def BinomialDist():
-	print(st.dbinom(x=2, size=5, prob=0.3))
-	print(st.dbinom(x=[7, 8, 9], size=9, prob=0.94))
-	print(st.qbinom(p=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6], size=9, prob=0.65))
-
-
-def NegativeBinomialDist():
-	print(st.dnbinom(x=2, size=5, prob=0.3))
-	print(st.dnbinom(x=[7, 8, 9], size=9, prob=0.94))
-
-	print(st.pnbinom(q=4, size=6, prob=0.8))
-
-	print(st.qnbinom(p=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6], size=9, prob=0.65))
 
 
 def GeometricDist():
+	print("\n Geometric Dist")
 	print(st.dgeom(x=3, prob=0.6))
 	print(st.dgeom(x=[0,1,2,3], prob=0.6))
 
@@ -57,7 +60,9 @@ def GeometricDist():
 	print(st.qgeom(p=[0,0.1,0.2,0.3, 0.5, 0.7], prob=0.45))
 
 
+
 def HyperGeometricDist():
+	print("\n Hypergeometric Dist")
 	print(st.dhyper(x=3, m=10, n=4, k=5))
 	print(st.phyper(q=3, m=10, n=4, k=5))
 	print(st.qhyper(p=[i*0.1 for i in range(10)], m=10, n=4, k=5))
@@ -65,8 +70,9 @@ def HyperGeometricDist():
 
 
 
-def dmultinom():
-	from scisuit.stats import dmultinom
+def multinomdist():
+	from scisuit.stats import dmultinom, rmultinom
+	print("\n Multinomial Dist")
 
 	probs = [1/21*i for i in range(1,7)]
 	x=[2]*6
@@ -74,9 +80,6 @@ def dmultinom():
 	p = dmultinom(x=x, size=12, prob=probs)
 	print(f"probability={p}")
 
-
-def rmultinom():
-	from scisuit.stats import rmultinom
 
 	n=10
 
@@ -95,7 +98,22 @@ def rmultinom():
 	print(f"Difference = {means - E_X}")
 
 
+
+
+def NegativeBinomialDist():
+	print("\n Negative Binomial Dist")
+	print(st.dnbinom(x=2, size=5, prob=0.3))
+	print(st.dnbinom(x=[7, 8, 9], size=9, prob=0.94))
+
+	print(st.pnbinom(q=4, size=6, prob=0.8))
+
+	print(st.qnbinom(p=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6], size=9, prob=0.65))
+
+
+
+
 def weibull():
+	print("\n Weibull Dist")
 	print(st.dweibull(x=3, shape=2, scale=4))
 	print(st.pweibull(q=3, shape=2, scale=4))
 	print(st. qweibull(p=0.3, shape=2, scale=4))
@@ -146,12 +164,16 @@ def gamma_weibull_shape():
 
 
 
-def beta():
-	from scisuit.stats import dbeta, pbeta, qbeta
-
-	print(dbeta(x=0.1, shape1=0.5, shape2=1))
-	print(pbeta(q=0.1, shape1=0.5, shape2=1))
-	print(qbeta(p=0.1, shape1=0.5, shape2=1))
 
 
 
+
+beta()
+BinomialDist()
+ExponentialDist()
+GammaDist()
+GeometricDist()
+HyperGeometricDist()
+NegativeBinomialDist()
+multinomdist()
+weibull()
