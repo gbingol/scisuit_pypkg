c157e592303f70c6a3390d582ca35b3353bc776a
Gökhan Bingöl <gbingol@hotmail.com>
2024-07-19 14:28:38 +0200

    more readable setup

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages, Distribution
 import platform
 import sys
 
+
+
 Distribution.has_ext_modules=lambda *args: True
 
 
@@ -28,16 +30,16 @@ def get_python_abi_tag():
 
 
 setup(
-	name='scisuit',
-	version='1.3.5',
-	author='Gokhan Bingol, PhD',
-	author_email='gbingol@pebytes.com',
-	description='Scientific Computing Package',
-	long_description=open('README.md').read(),
-	long_description_content_type='text/markdown',
-	license='GPLv3',
-	url='https://www.pebytes.com',
-	classifiers=[
+	name = 'scisuit',
+	version = '1.3.5',
+	author = 'Gokhan Bingol, PhD',
+	author_email = 'gbingol@pebytes.com',
+	description = 'Scientific Computing Package',
+	long_description = open('README.md').read(),
+	long_description_content_type = 'text/markdown',
+	license = 'GPLv3',
+	url = 'https://www.pebytes.com',
+	classifiers = [
 		'Programming Language :: Python :: 3',
 		'Programming Language :: C++',
 		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
@@ -47,11 +49,11 @@ setup(
 		'Intended Audience :: Science/Research',
 		'Topic :: Scientific/Engineering'
 	],
-	python_requires='>=3.10',
-	install_requires=['numpy'], #no need to use limitation on numpy 'numpy<=2.0'
-	packages=find_packages(),
-	package_data={'': ['README.md'], 'scisuit':['*.dll']},
-	setup_requires=['setuptools>=61.0'],
+	python_requires = '>=3.10',
+	install_requires = ['numpy'], #no need to use limitation on numpy 'numpy<=2.0'
+	packages = find_packages(),
+	package_data = {'': ['README.md'], 'scisuit':['*.dll']}, #key is folder name
+	setup_requires = ['setuptools>=61.0'],
 	options={
 		'bdist_wheel': {
 			'python_tag': get_python_abi_tag(),  # Specify the Python ABI tag
