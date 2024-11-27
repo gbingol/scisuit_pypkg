ca2feefb38e7dc4a4b4d7c8efc73ed8df82b518e
gbingol <gbingol@hotmail.com>
2024-11-27 16:42:55 +0300

    edited docstrings and changed imports

--- scisuit/optimize.py
+++ scisuit/optimize.py
@@ -1,6 +1,6 @@
-import dataclasses as _dc
-import numbers as _numbers
-import types as _types
+from dataclasses import dataclass
+from numbers import Real
+from types import FunctionType
 
 from ctypes import py_object, c_double, c_uint32, c_longlong
 from ._ctypeslib import pydll as _pydll
@@ -48,7 +48,7 @@ _pydll.c_optimize_brent.restype = py_object
 #-----------------------------------------------------------
 
 
-@_dc.dataclass
+@dataclass
 class BracketResult:
 	a:float
 	b:float
@@ -59,29 +59,28 @@ class BracketResult:
 
 
 def bracket(
-	f:_types.FunctionType, 
-	xa:_numbers.Real = 0.0, 
-	xb:_numbers.Real = 1.0, 
+	f:FunctionType, 
+	xa:Real = 0.0, 
+	xb:Real = 1.0, 
 	grow_limit=110,
 	maxiter=1000)->BracketResult:
 	"""
-	Bracket the minimum of a function.
+	Bracket the minimum of a function.  
 	Returns three points that bracket the minimum of the function.
 
-	## Inputs:
-	f: A unary function
-	xa, xb: Initial guesses, local minimum need not be contained within this interval.
-	grow_limit: Maximum grow limit.
-	maxiter: Maximum number of iterations
+	f: A unary function  
+	xa, xb: Initial guesses, local minimum need not be contained within this interval.  
+	grow_limit: Maximum grow limit.  
+	maxiter: Maximum number of iterations  
 
 	## References:
 	- Press WH, Teukolsky SA, Vetterling WT, Flannery BP (2007). 
 	  Numerical Recipes The Art of Scientific Computing. Cambridge Uni Press.
 	"""
-	assert isinstance(f, _types.FunctionType), "f must be function"
-	assert isinstance(xa, _numbers.Real), "xa must be real number"
-	assert isinstance(xb, _numbers.Real), "xb must be real number"
-	assert isinstance(grow_limit, _numbers.Real), "grow_limit must be real number"
+	assert isinstance(f, FunctionType), "f must be function"
+	assert isinstance(xa, Real), "xa must be real number"
+	assert isinstance(xb, Real), "xb must be real number"
+	assert isinstance(grow_limit, Real), "grow_limit must be real number"
 	assert isinstance(maxiter, int), "maxiter must be int"
 
 	assert maxiter>0, "maxiter>0 expected"
@@ -98,7 +97,7 @@ def bracket(
 
 #-------------------------------------------------------------------
 
-@_dc.dataclass
+@dataclass
 class GoldenResult:
 	xopt:float
 	err: float
@@ -106,29 +105,28 @@ class GoldenResult:
 
 
 def golden(
-	f:_types.FunctionType, 
-	xlow:_numbers.Real, 
-	xhigh:_numbers.Real, 
+	f:FunctionType, 
+	xlow:Real, 
+	xhigh:Real, 
 	tol=1E-6,
 	maxiter=1000)->GoldenResult:
 	"""
 	Finds the *local* minimum using golden section method
 
-	## Inputs:
-	f: A unary function
-	xlow, xhigh: Initial guesses, local minimum need to be contained within this interval.
-	tol: tolerance.
-	maxiter: Maximum number of iterations
+	f: A unary function  
+	xlow, xhigh: Initial guesses, local minimum need to be contained within this interval.  
+	tol: tolerance.  
+	maxiter: Maximum number of iterations  
 
 	## References:
 	- Chapra SC, Canale RP (2013). Numerical Methods for Engineers, 7th Ed. McGraw Hill Education.
 	- Press WH, Teukolsky SA, Vetterling WT, Flannery BP (2007). 
 	  Numerical Recipes The Art of Scientific Computing. Cambridge University Press.
 	"""
-	assert isinstance(f, _types.FunctionType), "f must be function"
-	assert isinstance(xlow, _numbers.Real), "xlow must be real number"
-	assert isinstance(xhigh, _numbers.Real), "xhigh must be real number"
-	assert isinstance(tol, _numbers.Real), "tol must be real number"
+	assert isinstance(f, FunctionType), "f must be function"
+	assert isinstance(xlow, Real), "xlow must be real number"
+	assert isinstance(xhigh, Real), "xhigh must be real number"
+	assert isinstance(tol, Real), "tol must be real number"
 	assert isinstance(maxiter, int), "maxiter must be int"
 
 	assert maxiter>0, "maxiter>0 expected"
@@ -146,7 +144,7 @@ def golden(
 
 #-----------------------------------------------------------------
 
-@_dc.dataclass
+@dataclass
 class BrentResult:
 	xopt:float
 	fval: float
@@ -154,24 +152,23 @@ class BrentResult:
 
 
 def brent(
-	f:_types.FunctionType, 
-	xlow:_numbers.Real, 
-	xhigh:_numbers.Real, 
+	f:FunctionType, 
+	xlow:Real, 
+	xhigh:Real, 
 	maxiter=500)->BrentResult:
 	"""
 	Finds the minimum using Brent's method
 
-	## Inputs:
-	f: A unary function
-	xlow, xhigh: Initial guesses, local minimum need to be contained within this interval.
+	f: A unary function,  
+	xlow, xhigh: Initial guesses, local minimum need to be contained within this interval,  
 	maxiter: Maximum number of iterations
 
 	## References
 	- https://www.boost.org/doc/libs/1_82_0/libs/math/doc/html/math_toolkit/brent_minima.html
 	"""
-	assert isinstance(f, _types.FunctionType), "f must be function"
-	assert isinstance(xlow, _numbers.Real), "xlow must be real number"
-	assert isinstance(xhigh, _numbers.Real), "xhigh must be real number"
+	assert isinstance(f, FunctionType), "f must be function"
+	assert isinstance(xlow, Real), "xlow must be real number"
+	assert isinstance(xhigh, Real), "xhigh must be real number"
 	assert isinstance(maxiter, int), "maxiter must be int"
 
 	assert maxiter>0, "maxiter>0 expected"
@@ -187,7 +184,7 @@ def brent(
 
 #----------------------------------------------------------
 
-@_dc.dataclass
+@dataclass
 class ParabolicResult:
 	xopt:float
 	err: float
@@ -195,36 +192,35 @@ class ParabolicResult:
 
 
 def parabolic(
-	f:_types.FunctionType, 
-	xa:_numbers.Real, 
-	xb:_numbers.Real, 
-	xc:None | _numbers.Real = None,
+	f:FunctionType, 
+	xa:Real, 
+	xb:Real, 
+	xc:None | Real = None,
 	tol=1E-6,
 	maxiter=1000)->ParabolicResult:
 	"""
 	Finds the minimum using quadratic interpolation algorithm
 
-	## Inputs:
-	f: A unary function
-	xa, xb, xc: initial guesses
-	tol: tolerance.
-	maxiter: Maximum number of iterations
+	f: A unary function,  
+	xa, xb, xc: initial guesses,  
+	tol: tolerance,  
+	maxiter: Maximum number of iterations  
 
 	## References: 
 	- Chapra SC, Canale RP (2013). Numerical Methods for Engineers, 7th Ed. McGraw Hill Education.
 	- Cheney W, Kincaid D (2007). Numerical Mathematics and Computing, 6th Ed. Brooks Cole 
 	"""
-	assert isinstance(f, _types.FunctionType), "f must be function"
-	assert isinstance(xa, _numbers.Real), "xa must be real"
-	assert isinstance(xb, _numbers.Real), "xb must be real"
-	assert isinstance(xc, _numbers.Real | None), "xc must be None or real"
-	assert isinstance(tol, _numbers.Real), "tol must be real"
+	assert isinstance(f, FunctionType), "f must be function"
+	assert isinstance(xa, Real), "xa must be real"
+	assert isinstance(xb, Real), "xb must be real"
+	assert isinstance(xc, Real | None), "xc must be None or real"
+	assert isinstance(tol, Real), "tol must be real"
 	assert isinstance(maxiter, int), "maxiter must be int"
 
 	assert maxiter>0, "maxiter>0 expected"
 
 	assert abs(xa-xb)>tol, "xa and xb must be different"
-	if isinstance(xc, _numbers.Real):
+	if isinstance(xc, Real):
 		assert abs(xc-xb), "xa != xb != xc expected"
 
 	xopt, err, iter =_pydll.c_optimize_parabolic(py_object(f), 
