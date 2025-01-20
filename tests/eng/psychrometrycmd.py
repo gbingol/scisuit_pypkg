747fbbb6f83f9179fa187efdfc0ecc84e88b7be9
Gökhan Bingöl <gbingol@hotmail.com>
2025-01-20 11:07:28 +0100

    update psychrometrycmd test cases with new parameters

--- tests/eng/psychrometrycmd.py
+++ tests/eng/psychrometrycmd.py
@@ -1,10 +1,14 @@
 from scisuit.eng import psychrometry
 
 #linear combination
-result = psychrometry(P=101, Tdb=70, Twb=40)
-
-#testing new solver method
-result2 = psychrometry(RH=result.RH, H=result.H, P=result.P)
-
-print(dict(result))
+result = psychrometry(P=101, Tdb=20, Twb=15)
 print(result)
+
+
+#result2 = psychrometry(Twb=result.Twb, H=result.H, Tdp=result.Tdp)
+#result2 = psychrometry(V=result.V, Twb=result.Twb, Tdp=result.Tdp)
+
+#ValueError: W < 0.0
+result2 = psychrometry(V=result.V, RH=result.RH, Tdb=result.Tdb)
+
+print(result2)
