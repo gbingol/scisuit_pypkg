2350d8ee1a33eb39fea908bad010114a8042bc1c
Gökhan Bingöl <gbingol@hotmail.com>
2025-01-20 12:07:40 +0100

    bug fixed therefore removed ValueError line in psychrometrycmd test

--- tests/eng/psychrometrycmd.py
+++ tests/eng/psychrometrycmd.py
@@ -8,7 +8,6 @@ print(result)
 #result2 = psychrometry(Twb=result.Twb, H=result.H, Tdp=result.Tdp)
 #result2 = psychrometry(V=result.V, Twb=result.Twb, Tdp=result.Tdp)
 
-#ValueError: W < 0.0
 result2 = psychrometry(V=result.V, RH=result.RH, Tdb=result.Tdb)
 
 print(result2)
