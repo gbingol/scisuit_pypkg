11aeea89fefc334ee1574a9a430906c1191e5393
Gökhan Bingöl <gbingol@hotmail.com>
2025-03-13 12:28:12 +0100

    update: modify psychrometry command parameters for testing

--- tests/psychrometrycmd.py
+++ tests/psychrometrycmd.py
@@ -8,6 +8,6 @@ print(result)
 #result2 = psychrometry(Twb=result.Twb, H=result.H, Tdp=result.Tdp)
 #result2 = psychrometry(V=result.V, Twb=result.Twb, Tdp=result.Tdp)
 
-result2 = psychrometry(V=result.V, RH=result.RH, Tdb=result.Tdb)
+result2 = psychrometry(V=0.8, RH=60, P=101)
 
 print(result2)
