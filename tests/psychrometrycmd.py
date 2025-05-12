e55cf0c69907226711af6473ef51072c48dcad88
gbingol <gbingol@hotmail.com>
2025-05-12 21:54:04 +0200

    refactor: improve psychrometry test script for clarity and organization

--- tests/psychrometrycmd.py
+++ tests/psychrometrycmd.py
@@ -1,13 +1,19 @@
 from scisuit.eng import psychrometry
+import scisuit.plot as plt
+import scisuit.plot.gdi as gdi
 
-#linear combination
-result = psychrometry(P=101, Tdb=20, Twb=15)
-print(result)
+psy = psychrometry(P=101, Tdb=20, Twb=15)
+print(psy)
 
+P = 101
+Tdb = 20
+W = 0.008623540951296561
 
-#result2 = psychrometry(Twb=result.Twb, H=result.H, Tdp=result.Tdp)
-#result2 = psychrometry(V=result.V, Twb=result.Twb, Tdp=result.Tdp)
+#Instead of using theoretical limits we are using the practical ones
+assert 70<P<120, "P [70, 120] kPa expected."
+assert 0<W<1, "Absolute humidity (0, 1) expected."
+assert -0<Tdb<90, "Tdb [0, 90]"
 
-result2 = psychrometry(V=0.8, RH=60, P=101)
-
-print(result2)
+plt.psychrometry(P=P*1000)
+gdi.marker(xy=(Tdb, W), size=5, fc="#000000")
+plt.show()
