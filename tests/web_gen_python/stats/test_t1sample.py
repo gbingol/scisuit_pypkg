a3185716eb9c050c801a20580b3d486175cb9039
Gökhan Bingöl <gbingol@hotmail.com>
2025-05-15 16:57:04 +0200

    feat: add test_tpaired.py for paired t-test implementation and visualization

--- tests/web_gen_python/stats/test_t1sample.py
+++ tests/web_gen_python/stats/test_t1sample.py
@@ -1,6 +1,6 @@
 import numbers
 import scisuit.plot as plt
-from scisuit.stats import test_t
+from scisuit.stats import test_t, test_t1_result
 
 #Inputs
 conflevel = 0.95
@@ -15,7 +15,7 @@ Data = [0.69,0.606,0.57,0.749,0.672,0.628,0.609,0.844,0.654,0.615,0.668,0.601,0.
 Data = [i for i in Data if isinstance(i, numbers.Real)]
 assert len(Data)>2, "At least 3 data points expected"
 
-Result = test_t(x=Data, mu=Mu, alternative = Alternative, conflevel = conflevel)
+Result:test_t1_result = test_t(x=Data, mu=Mu, alternative = Alternative, conflevel = conflevel)
 print(Result)
 
 #For visualization purposes
--- tests/web_gen_python/stats/test_tpaired.py
+++ tests/web_gen_python/stats/test_tpaired.py
@@ -1,25 +1,25 @@
 import numbers
 import scisuit.plot as plt
-from scisuit.stats import test_t, test_t2_result
+from scisuit.stats import test_t, test_tpaired_result
 
 #Inputs
 conflevel = 0.95 #confidence level
 MeanDiff = 0 #Assumed mean difference
-EqualVars = True #Equal variances?
 
 AlterOpt = ["two.sided", "greater", "less"]
 Alternative = AlterOpt[0]
 
-xdata = [24,43,58,71,43,49,61,44,67,49,53,56,59,52,62,54,57,33,46,43,57]
-ydata = [42,43,55,54,20,85,33,41,19,60,53,42,46,10,17,28,48,37,42,55,26,62,37]
+xdata = [18,21,16,22,19,24,17,21,23,18,14,16,16,19,18,20,12,22,15,17]
+ydata = [22,25,17,24,16,29,20,23,19,20,15,15,18,26,18,24,18,25,19,16]
 
 xdata = [i for i in xdata if isinstance(i, numbers.Real)]
 ydata = [i for i in ydata if isinstance(i, numbers.Real)]
 
+assert len(xdata) == len(ydata), "Sample #1 and #2 must have same lengths"
 assert len(xdata)>2, "At least 3 data points expected"
 assert len(ydata)>2, "At least 3 data points expected"
 
-Result:test_t2_result = test_t(x=xdata, y=ydata, mu=MeanDiff, varequal=EqualVars, alternative=Alternative, conflevel=conflevel)
+Result:test_tpaired_result = test_t(x=xdata, y=ydata, mu=MeanDiff, paired=True, alternative=Alternative, conflevel=conflevel)
 print(Result)
 
 #For visualization purposes
