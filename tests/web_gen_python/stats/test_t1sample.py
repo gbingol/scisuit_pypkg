43d8482e26b2ef6db8087e744a99ad0c810994c7
Gökhan Bingöl <gbingol@hotmail.com>
2025-05-15 12:38:55 +0200

    added 1-sample t-test (web generated)

---
+++ tests/web_gen_python/stats/test_t1sample.py
@@ -0,0 +1,26 @@
+import numbers
+import scisuit.plot as plt
+from scisuit.stats import test_t
+
+#Inputs
+conflevel = 0.95
+Mu = 0.618
+
+AlterOpt = ["two.sided", "greater", "less"]
+Alternative = AlterOpt[0]
+
+Data = [0.69,0.606,0.57,0.749,0.672,0.628,0.609,0.844,0.654,0.615,0.668,0.601,0.576,0.67,0.606,0.611,0.553,0.933]
+
+#Ensure Data contains only numbers and at least 3 of them
+Data = [i for i in Data if isinstance(i, numbers.Real)]
+assert len(Data)>2, "At least 3 data points expected"
+
+Result = test_t(x=Data, mu=Mu, alternative = Alternative, conflevel = conflevel)
+print(Result)
+
+#For visualization purposes
+plt.hist(Data)
+plt.figure()
+plt.boxplot(Data)
+			
+plt.show()
