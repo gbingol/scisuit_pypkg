a33c989dba7e8187bd1375c0d7ce929866bc732e
Gökhan Bingöl <gbingol@hotmail.com>
2025-05-31 12:05:25 +0200

    refactor: update argument types for nonparametric sign test and improve function signature

--- scisuit/stats/basictests/_nonparametric.py
+++ scisuit/stats/basictests/_nonparametric.py
@@ -3,11 +3,11 @@ import numbers
 from dataclasses import dataclass
 from typing import Iterable
 
-from ctypes import py_object, c_double, c_char_p
+from ctypes import py_object, c_double, c_char_p, c_bool
 from ..._ctypeslib import pydll as _pydll
 
-_pydll.c_stat_test_nonparam_signtest.argtypes = [py_object, c_double, c_double, c_char_p]
-_pydll.c_stat_test_nonparam_signtest.restype = py_object
+_pydll.c_stat_nonparam_signtest.argtypes = [py_object, c_double, c_bool, c_double, c_char_p]
+_pydll.c_stat_nonparam_signtest.restype = py_object
 
 
 
@@ -30,13 +30,15 @@ class test_sign_Result:
 def test_sign(
 		x:Iterable, 
 		md:numbers.Real,  
+		confint=True,
 		alternative="two.sided", 
-		conflevel=0.95)->tuple[float, test_sign_Result]:
+		conflevel=0.95)->test_sign_Result:
 	"""
-	returns p-value and a test_sign_Result class.  
+	returns test_sign_Result class.  
 
 	x: Sample
 	md: Median of the population tested by the null hypothesis  
+	confint: Should compute confidence intervals?  
 	alternative: "two.sided", "less", "greater"   
 	conflevel:	Confidence level, [0,1]  
 	"""
@@ -44,16 +46,19 @@ def test_sign(
 	assert conflevel>0.0 or conflevel<1.0, "conflevel must be in range (0, 1)"
 	assert isinstance(x, Iterable), "x must be Iterable"
 	assert isinstance(md, numbers.Real), "md must be real number"
+	assert isinstance(confint, bool), "confint must be bool"
 	assert isinstance(alternative, str), "alternative must be str"
 
 	assert alternative in ["two.sided", "less", "greater"], "alternative must be two.sided, less or greater"
 
-	dct =  _pydll.c_stat_test_nonparam_signtest(x, c_double(md), c_double(conflevel), c_char_p(alternative.encode()))
-
-	return test_sign_Result(
-		lower=(dct["acl1"], dct["ci1_first"], dct["ci1_second"]),
-		upper=(dct["acl2"], dct["ci2_first"], dct["ci2_second"]),
-		interpolated=(dct["ici_first"], dct["ici_second"]),
-		pvalue=dct["pvalue"])
-
+	dct =  _pydll.c_stat_nonparam_signtest(x, c_double(md), c_bool(confint), c_double(conflevel), c_char_p(alternative.encode()))
+	
+	if confint:
+		return test_sign_Result(
+			lower=(dct["acl1"], dct["ci1_first"], dct["ci1_second"]),
+			upper=(dct["acl2"], dct["ci2_first"], dct["ci2_second"]),
+			interpolated=(dct["ici_first"], dct["ici_second"]),
+			pvalue=dct["pvalue"])
+	
+	return test_sign_Result(pvalue=dct["pvalue"])
 
