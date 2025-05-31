4e29508b71b7192b854569e3c9a25cb87f8ddf58
Gökhan Bingöl <gbingol@hotmail.com>
2025-05-31 14:21:02 +0200

    feat: add initial test for test_wilcox function with sample data

---
+++ tests/stats/nonparam/wilcoxsigntest.py
@@ -0,0 +1,13 @@
+from scisuit.stats import test_wilcox
+
+data1 = {
+	"x":[7.8, 6.6, 6.5, 7.4, 7.3, 7., 6.4, 7.1, 6.7, 7.6, 6.8], 
+	"md":6.5, "confint":True}
+
+data2 = {
+	"x":[24, 28, 28, 31, 32, 33, 38, 39, 41, 43, 48, 49, 49, 51, 52, 56, 56, 58, 62, 63],
+	"md": 40}
+
+
+result = test_wilcox(**data1)
+print(result)
