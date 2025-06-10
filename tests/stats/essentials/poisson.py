ba1517d24b03264257d6369889986639f9e9f71e
Gökhan Bingöl <gbingol@hotmail.com>
2025-06-10 14:21:48 +0200

    refactor: streamline test_poisson1sample calls in samples_freq and summarizeddata functions

--- tests/stats/essentials/poisson.py
+++ tests/stats/essentials/poisson.py
@@ -4,17 +4,35 @@ def samples_freq():
 	Sample = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 	Freq = [57, 203, 383, 525, 532, 408, 273, 139, 45, 27, 10, 6]
 		
-	res = test_poisson1sample(Sample, Freq, None, None, 1.0, True, 4.0, 0.95, "normal", "two.sided")
-	print(res)
+	return test_poisson1sample(
+				Sample, 
+				Freq, 
+				length=1.0, 
+				hypotest=True, 
+				hyporate=4.0, 
+				conflevel=0.95, 
+				method="normal", 
+				alternative="two.sided")
+	
 
 
 def summarizeddata():
 	samplesize = 2608
 	totaloccur = 10092
 
-	res = test_poisson1sample(None, None, samplesize, totaloccur, 1.0, True, 4.0, 0.95, "normal", "two.sided")
-	print(res)
+	return test_poisson1sample(
+						samplesize=samplesize, 
+						totaloccur=totaloccur, 
+						length=1.0, 
+						hypotest=True, 
+						hyporate=4.0, 
+						conflevel=0.95, 
+						method="normal", 
+						alternative="two.sided")
 
 
-samples_freq()
-summarizeddata()
+print(samples_freq())
+
+print("\n------------\n")
+
+print(summarizeddata())
