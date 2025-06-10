9470db608a1abdd37c7bdfba66514b24d221bd61
Gökhan Bingöl <gbingol@hotmail.com>
2025-06-10 14:32:58 +0200

    refactor: rename functions for clarity and update print statements

--- tests/stats/essentials/poisson.py
+++ tests/stats/essentials/poisson.py
@@ -1,6 +1,6 @@
 from scisuit.stats import test_poisson1sample, test_poisson1sample_Result
 
-def samples_freq():
+def volcanoerupt():
 	Sample = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 	Freq = [57, 203, 383, 525, 532, 408, 273, 139, 45, 27, 10, 6]
 		
@@ -16,7 +16,9 @@ def samples_freq():
 	
 
 
-def summarizeddata():
+
+
+def volcanoerupt_summarizeddata():
 	samplesize = 2608
 	totaloccur = 10092
 
@@ -31,8 +33,30 @@ def summarizeddata():
 						alternative="two.sided")
 
 
-print(samples_freq())
+
+
+def customercomplaints():
+	days = range(1,31) #data collected
+	complaints = [19,18,22,21,17,18,22,19,16,23,25,16,18,18,
+			   20,21,15,23,21,19,26,
+			   21,17,26,16,24,21,18,17,21]
+	
+	return test_poisson1sample(
+				complaints,  
+				length=1, 
+				hypotest=True, 
+				hyporate=10, 
+				conflevel=0.95, 
+				method="normal", 
+				alternative="greater")
+
+
+print(volcanoerupt())
 
 print("\n------------\n")
 
-print(summarizeddata())
+print(volcanoerupt_summarizeddata())
+
+print("\n------------\n")
+
+print(customercomplaints())
