d47fc460047d07fcd5aa0883b29dbf1873d400d7
Gökhan Bingöl <gbingol@hotmail.com>
2025-06-19 18:02:43 +0200

    testing Tukey's post-hoc test

--- tests/stats/aov/aov1.py
+++ tests/stats/aov/aov1.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-from scisuit.stats import aov
+from scisuit.stats import aov, tukey
 
 A = np.array([16, 11, 20, 21, 14, 7])
 B = np.array([21, 12, 14, 17, 13, 17])
@@ -12,3 +12,25 @@ D = np.array([45, 59, 48, 46, 38, 47])
 result = aov(A, B, C, D)
 print(result)
 
+tukeyres = tukey(0.05, result)
+print(tukeyres)
+
+
+"""
+One-Way ANOVA Results 
+Source             df              SS              MS               F         p-value
+Treatment           3         4194.33         1398.11           27.23      2.9061e-07
+Error              20         1027.00           51.35
+Total              23         5221.33
+
+   Tukey Test Results (alpha=0.05)
+
+Pairwise Diff             i-j             Interval
+1 - 2                -0.83           (-12.41, 10.75)
+1 - 3               -16.17           (-27.75, -4.59)
+1 - 4               -32.33          (-43.91, -20.75)
+2 - 3               -15.33           (-26.91, -3.75)
+2 - 4               -31.50          (-43.08, -19.92)
+3 - 4               -16.17           (-27.75, -4.59)
+
+"""
