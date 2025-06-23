7b67e7f330be6cc56115162b97fa8b1d696d956e
Gökhan Bingöl <gbingol@hotmail.com>
2025-06-23 11:45:49 +0200

    refactor: replace numpy arrays with lists for input data in ANOVA test

--- tests/stats/aov/aov1.py
+++ tests/stats/aov/aov1.py
@@ -1,12 +1,9 @@
-import numpy as np
-
-
 from scisuit.stats import aov, tukey, fisher
 
-A = np.array([16, 11, 20, 21, 14, 7])
-B = np.array([21, 12, 14, 17, 13, 17])
-C = np.array([37, 32, 12, 25, 39, 41])
-D = np.array([45, 59, 48, 46, 38, 47])
+A = [16, 11, 20, 21, 14, 7]
+B = [21, 12, 14, 17, 13, 17]
+C = [37, 32, 12, 25, 39, 41]
+D = [45, 59, 48, 46, 38, 47]
 
 #perform 1-way ANOVA
 result = aov(A, B, C, D)
