ed449b5c1b8fdbbc5163550cda867981518de162
Gökhan Bingöl <gbingol@hotmail.com>
2025-06-29 19:33:24 +0200

    removed docstring as it is not needed anymore

--- tests/stats/aov/aov1.py
+++ tests/stats/aov/aov1.py
@@ -11,23 +11,3 @@ print(result)
 
 tukeyres = fisher(0.05, result)
 print(tukeyres)
-
-
-"""
-One-Way ANOVA Results 
-Source             df              SS              MS               F         p-value
-Treatment           3         4194.33         1398.11           27.23      2.9061e-07
-Error              20         1027.00           51.35
-Total              23         5221.33
-
-   Tukey Test Results (alpha=0.05)
-
-Pairwise Diff             i-j             Interval
-1 - 2                -0.83           (-12.41, 10.75)
-1 - 3               -16.17           (-27.75, -4.59)
-1 - 4               -32.33          (-43.91, -20.75)
-2 - 3               -15.33           (-26.91, -3.75)
-2 - 4               -31.50          (-43.08, -19.92)
-3 - 4               -16.17           (-27.75, -4.59)
-
-"""
