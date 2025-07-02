261889020aa8e1402391bec0b46faa4f01a14610
Gökhan Bingöl <gbingol@hotmail.com>
2025-07-02 11:26:27 +0200

    added settings for NDIGITS

--- scisuit/settings.py
+++ scisuit/settings.py
@@ -1,8 +1,16 @@
 
+
+START_APP_MAINLOOP = True
 """
 Apps embedding Python have their own mainloop.
 When using scisuit plotting functions there is no need to start another mainloop.
 
 Tested on apps built with wxWidgets.
 """
-START_APP_MAINLOOP = True
+
+
+
+NDIGITS = 3
+"""
+Number of decimal points to be used when rounding a floating number (used during outputs)
+"""
